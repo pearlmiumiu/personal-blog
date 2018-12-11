05bf5c1e26868ee60143db34ddf2c3975900296f
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2018-12-10 22:29:19 -0500

    my blog

---
+++ articles/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name='articles'
+urlpatterns = [
+ 
+   
+    url(r'^$', views.article_list, name="list"), #call back func
+    url(r'^create/$', views.article_create, name="create"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
+
+]
